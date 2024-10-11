d7871f56dbf1112d902ffe4f9f126ef32df67878
ruspian <ruspianntb@gmail.com>
2024-10-11 16:53:44 +0800

    menjalanjan fungsi copy link

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,16 +1,24 @@
 // mengubah sesi ini menjadi client component dengan use client
 "use client";
 
+import { useState } from "react";
+
 export default function ShareLinkButton() {
+  const [copied, setCopied] = useState(false);
   function handleClick() {
-    console.log("tombol di klik");
+    // mengambil link blog
+    navigator.clipboard.writeText(window.location.href);
+
+    setCopied(true);
+    // mengatur waktu mengcopy
+    setTimeout(() => setCopied(false), 1500);
   }
   return (
     <button
       onClick={handleClick}
       className="border px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700"
     >
-      Salin Link
+      {copied ? "Link Disalin" : "Bagikan Link"}
     </button>
   );
 }
