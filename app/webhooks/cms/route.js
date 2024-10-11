4ce0539cfaf4159c57e4a303309ea5f72aedd058
ruspian <ruspianntb@gmail.com>
2024-10-12 05:01:10 +0800

    menggunakan on demand revalidate

--- app/webhooks/cms/route.js
+++ app/webhooks/cms/route.js
@@ -1,8 +1,13 @@
+import { CACHE_TAG } from "@/lib/post";
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const payload = await req.json();
-  console.log(payload);
+
+  if (payload.model === "post") {
+    revalidateTag(CACHE_TAG);
+  }
 
   return new Response(null, { status: 204 });
 }
