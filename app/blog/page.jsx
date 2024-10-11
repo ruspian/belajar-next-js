e1e8b304cab1ae50dabd2222382685b3ffb59673
ruspian <ruspianntb@gmail.com>
2024-10-12 05:28:32 +0800

    memfungsikan pagination

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -11,17 +11,17 @@ export const metadata = {
 };
 
 export default async function BlogPage({ searchParams }) {
-  const halaman = parsePageParams(searchParams.halaman);
-  const posts = await getAllPosts();
+  const page = parsePageParams(searchParams.page);
+  const posts = await getAllPosts(3, page);
   return (
     <>
       <Heading>Blog Saya</Heading>
       <p>List blog saya</p>
 
       <div className="flex gap-3 pb-3">
-        <Link href={`/blog?halaman=${halaman - 1}`}>&lt;</Link>
-        <span>Halaman {halaman}</span>
-        <Link href={`/blog?halaman=${halaman + 1}`}>&gt;</Link>
+        <Link href={`/blog?page=${page - 1}`}>&lt;</Link>
+        <span>page {page}</span>
+        <Link href={`/blog?page=${page + 1}`}>&gt;</Link>
       </div>
 
       {/* looping data blog yang ada di folder content/blog */}
@@ -41,10 +41,10 @@ export default async function BlogPage({ searchParams }) {
 }
 
 function parsePageParams(paramValue) {
-  const halaman = parseInt(paramValue);
+  const page = parseInt(paramValue);
   if (paramValue) {
-    if (isFinite(halaman) && halaman > 0) {
-      return halaman;
+    if (isFinite(page) && page > 0) {
+      return page;
     }
   }
   return 1;
